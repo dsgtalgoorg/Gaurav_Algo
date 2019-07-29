cf1cbb851af13f0603f54fd8f973074166244e0f
Tyagi123 <gauravtyagi888@gmail.com>
2019-07-28 17:43:46 -0700

    StringRotation

---
+++ src/com/src/StringRotation.java
@@ -0,0 +1,40 @@
+
+public class StringRotation {
+
+    public static void main(String[] args) {
+        System.out.println(findStringRotation("WaterBottle", "erBottleWat"));
+        System.out.println(findStringRotation("WaterBottle", "BottleWater"));
+        System.out.println(findStringRotation("WaterBottle45", "BottleWater"));
+        System.out.println(findStringRotation("WaterBot5", "BottleWater"));
+
+        System.out.println("=============================================================");
+
+        System.out.println(findStringRotationWithConcatingString("WaterBottle", "erBottleWat"));
+        System.out.println(findStringRotationWithConcatingString("WaterBottle", "BottleWater"));
+        System.out.println(findStringRotationWithConcatingString("WaterBottle45", "BottleWater"));
+        System.out.println(findStringRotationWithConcatingString("WaterBot5", "BottleWater"));
+    }
+
+    //by normal comparision
+    private static boolean findStringRotation(String str, String rstr) {
+        char firstChar = rstr.charAt(0);
+        int len = str.length();
+        String s1 = null;
+        for (int i = 0; i < len; i++) {
+            if (str.charAt(i) == firstChar) {
+                s1 = str.substring(i, len).concat(str.substring(0, i));
+                if (s1.equals(rstr)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    //by normal comparision
+    private static boolean findStringRotationWithConcatingString(String str, String rstr) {
+        rstr = rstr + rstr;
+        return rstr.contains(str);
+    }
+}
