76cfeb96a22a669fbe9daaecc234377d946dcfcb
Tyagi123 <gauravtyagi888@gmail.com>
2019-07-28 18:23:09 -0700

    URLify

---
+++ src/com/src/URLify.java
@@ -0,0 +1,38 @@
+//"Mr John Smith    " would be Mr%20John%20Smith (String will always sufficient to %20)
+
+
+public class URLify {
+
+    public static void main(String[] str) {
+        urlifyString("Mr John Smith    ".toCharArray(), 12);
+    }
+
+    private static void urlifyString(char[] str, int current) {
+        int last = str.length - 1;
+        while (current >= 0) {
+            if (str[current] == ' ') {
+                addSpace(str, last);
+                last = last - 3;
+            } else {
+                str[last] = str[current];
+                --last;
+            }
+            --current;
+        }
+
+        //printing
+        for (char c : str) {
+            if (c == ' ') {
+                System.out.print("9");
+            }
+            System.out.print(c);
+        }
+    }
+
+
+    private static void addSpace(char[] str, int last) {
+        str[last] = '0';
+        str[last - 1] = '2';
+        str[last - 2] = '%';
+    }
+}
