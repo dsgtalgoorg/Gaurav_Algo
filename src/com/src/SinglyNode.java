9464fd513c5069397cceba8eb4d4019d25ee18ac
Tyagi123 <gauravtyagi888@gmail.com>
2019-07-30 00:08:28 -0700

    SinglyNode - added pojo class for singly linked list

---
+++ src/com/src/SinglyNode.java
@@ -0,0 +1,52 @@
+public class SinglyNode {
+
+    public SinglyNode next;
+    public Integer value;
+
+    public SinglyNode() {
+    }
+
+    public SinglyNode(SinglyNode next, Integer value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public static void printList(SinglyNode node) {
+        System.out.println("===============================");
+        while (node != null) {
+            System.out.println(node.value);
+            node = node.next;
+        }
+    }
+
+    public static SinglyNode addLinkedList(SinglyNode node, Integer... values) {
+        SinglyNode previousNode = null;
+        for (Integer value : values) {
+            SinglyNode localNode = new SinglyNode(null, value);
+            if (previousNode == null) {
+                node.next = localNode;
+                previousNode = localNode;
+            } else {
+                previousNode.next = localNode;
+                previousNode = localNode;
+            }
+        }
+        return node;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public SinglyNode getNext() {
+        return next;
+    }
+
+    public void setNext(SinglyNode next) {
+        this.next = next;
+    }
+}
