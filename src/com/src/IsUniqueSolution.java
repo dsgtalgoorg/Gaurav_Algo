4b1650d870998a0542619b789dd3ad4492b4a3bb
Dan Sajjad <dan.sajjad@gmail.com>
2019-07-29 19:46:32 -0700

    Green - isUnique (using a Set)

--- src/com/src/IsUniqueSolution.java
+++ src/com/src/IsUniqueSolution.java
@@ -1,26 +1,38 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
 public class IsUniqueSolution {
 
     public static boolean isUnique(String inputString) {
-        boolean[] charCodeArray = new boolean[26];
+//        boolean[] charCodeArray = new boolean[26];
+//
+//        for (char character : inputString.toCharArray()) {
+//            int unicodeValue = (int) Character.toLowerCase(character) - 97;
+//
+//            if (charCodeArray[unicodeValue])
+//                return false;
+//            else
+//                charCodeArray[unicodeValue] = true;
+//        }
+//        return true;
 
+
+        /*
+            USING SET
+         */
+
+        if (inputString.length() > 128) return false;
+        Set<Character> charSet = new HashSet<>();
         for (char character : inputString.toCharArray()) {
-            int unicodeValue = (int) Character.toLowerCase(character) - 97;
-
-            if (charCodeArray[unicodeValue])
-                return false;
-            else
-                charCodeArray[unicodeValue] = true;
+            if (charSet.contains(character)) return false;
+            else charSet.add(character);
         }
         return true;
     }
@@ -32,9 +44,29 @@ public class IsUniqueSolution {
      */
 
 
+
+    @Test
+    public void emptyStringTest() {
+        final String text = "";
+        assertTrue(isUnique(text));
+    }
+
+    @Test
+    public void singeltonTest() {
+        final String text = "c";
+        assertTrue(isUnique(text));
+    }
+
+
     @Test
     public void failureTest() {
         final String text = "Splinters";
+        assertFalse(isUnique(text));
+    }
+
+    @Test
+    public void failure2Test() {
+        final String text = "snazzy";
         assertFalse(isUnique(text));
     }
 
