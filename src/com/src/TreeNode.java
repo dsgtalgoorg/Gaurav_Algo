9a24b3c6ad7624af017a37e0c7e30b820e9a2f44
Tyagi123 <gauravtyagi888@gmail.com>
2019-08-10 08:41:26 -0700

    Added Treenode class

---
+++ src/com/src/TreeNode.java
@@ -0,0 +1,28 @@
+public class TreeNode {
+    Integer value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(Integer value) {
+        this.value = value;
+    }
+
+    public TreeNode(Integer value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static void printAll(TreeNode root) {
+        System.out.println("=================================================");
+        if (root == null) return;
+        System.out.println("Root Node " + root.value);
+        if (root.left != null)
+            System.out.println("Left Node " + root.left.value);
+        if (root.right != null)
+            System.out.println("Right Node " + root.right.value);
+        System.out.println("=================================================");
+        printAll(root.left);
+        printAll(root.right);
+    }
+}
