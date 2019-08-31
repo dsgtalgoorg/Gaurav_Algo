83ace80c89ab8244e6204c683985521552dc5bc4
Tyagi123 <gauravtyagi888@gmail.com>
2019-08-31 14:26:41 -0700

    MatrixObject

---
+++ src/com/src/MatrixObject.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class MatrixObject {
+
+    private int row, col;
+
+    public MatrixObject() {
+    }
+
+    public MatrixObject(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MatrixObject that = (MatrixObject) o;
+        return row == that.row &&
+                col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+}
